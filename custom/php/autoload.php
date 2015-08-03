04a083dbba541a61be851d8a0e3556786e23a93a
altbdoor <lancersupraskyline@gmail.com>
2015-08-03 10:09:02 +0800

    updated autoload.php to fix eaccelerator issues

--- custom/php/autoload.php
+++ custom/php/autoload.php
@@ -1,7 +1,11 @@
 <?php
 
-// custom app autoloader
-spl_autoload_register(function ($class) {
+/*
+	PLEASE NOTE THAT HOSTING CURRENTLY USES EACCELERATOR
+	AND IT DOES NOT SUPPORT LAMBDA FUNCTION CALLBACKS
+*/
+
+function autoload ($class) {
 	$class = str_replace('Custom\\', '', $class);
 	
 	$path = dirname(__FILE__).'/classes/'.$class.'.php';
@@ -9,4 +13,7 @@ spl_autoload_register(function ($class) {
 	if (file_exists($path)) {
 		require $path;
 	}
-});
+}
+
+// custom app autoloader
+spl_autoload_register('autoload');
